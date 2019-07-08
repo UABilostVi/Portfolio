6359936a1b71c3b58c61c554a5a9359c5be4e16b
UABilostViSTV <bilostotskyi1991@gmail.com>
2019-07-08 16:39:15 +0300

    add social networks

--- GoldenWay/src/js/main.js
+++ GoldenWay/src/js/main.js
@@ -13,3 +13,21 @@ function initMenu() {
 	};
 	opener.on('click', clickHandler);
 }
+
+
+$(document).ready(function(){
+    $(".drop").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+     $(".menu-opener").on("click", function(){
+        $("body").toggleClass("menu_active");
+        }); 
+});
